eb550366708d699b64854c0cca7034bb80ab86bb
secheng722 <826216325@qq.com>
2025-07-01 22:01:02 +0800

    Site updated: 2025-07-01 22:01:02

--- js/main.js
+++ js/main.js
@@ -54,28 +54,28 @@ $(document).ready(function() {
     /**
      * Add a scroll listener to the menu to hide/show the navigation links.
      */
-    if (menu.length) {
-      $(window).on("scroll", function() {
-        var topDistance = menu.offset().top;
+    // if (menu.length) {
+    //   $(window).on("scroll", function() {
+    //     var topDistance = menu.offset().top;
 
-        // hide only the navigation links on desktop
-        if (!nav.is(":visible") && topDistance < 50) {
-          nav.show();
-        } else if (nav.is(":visible") && topDistance > 100) {
-          nav.hide();
-        }
+    //     // hide only the navigation links on desktop
+    //     if (!nav.is(":visible") && topDistance < 50) {
+    //       nav.show();
+    //     } else if (nav.is(":visible") && topDistance > 100) {
+    //       nav.hide();
+    //     }
 
-        // on tablet, hide the navigation icon as well and show a "scroll to top
-        // icon" instead
-        if ( ! $( "#menu-icon" ).is(":visible") && topDistance < 50 ) {
-          $("#menu-icon-tablet").show();
-          $("#top-icon-tablet").hide();
-        } else if (! $( "#menu-icon" ).is(":visible") && topDistance > 100) {
-          $("#menu-icon-tablet").hide();
-          $("#top-icon-tablet").show();
-        }
-      });
-    }
+    //     // on tablet, hide the navigation icon as well and show a "scroll to top
+    //     // icon" instead
+    //     if ( ! $( "#menu-icon" ).is(":visible") && topDistance < 50 ) {
+    //       $("#menu-icon-tablet").show();
+    //       $("#top-icon-tablet").hide();
+    //     } else if (! $( "#menu-icon" ).is(":visible") && topDistance > 100) {
+    //       $("#menu-icon-tablet").hide();
+    //       $("#top-icon-tablet").show();
+    //     }
+    //   });
+    // }
 
     /**
      * Show mobile navigation menu after scrolling upwards,
